842070960594db28d933780f029b8ee968715887
Amir Mohammad Fakhimi <79265203+AmirMohammadFakhimi@users.noreply.github.com>
2021-07-15 15:34:23 +0430

    Delete src/main directory

--- src/main/module-info.java
+++
@@ -1,9 +0,0 @@
-module Yu.Gi.Oh.game {
-    requires javafx.controls;
-    requires javafx.media;
-    requires com.google.gson;
-    requires javafx.fxml;
-    requires opencsv;
-
-    exports view to javafx.graphics;
-}
